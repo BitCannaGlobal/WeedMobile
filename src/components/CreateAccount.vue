cf47047805b3c7939cae78fa7385018b9be9cad1
atmoner <contact.atmoner@gmail.com>
2023-09-20 17:47:14 +0200

    #4 Fix error display when new open

--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -284,6 +284,8 @@ import md5 from 'md5'
     methods: {    
       async openCreateWallet() {
         this.dialogCreateWallet = true
+        this.alertError = false
+        this.alertErrorName = false
         this.step = 1
         var generateSecret = await DirectSecp256k1HdWallet.generate(12)      
         this.generatedMnenomicPlain = generateSecret.secret.data
